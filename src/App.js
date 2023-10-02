160e62e09d26314e88057a4f7734e821af7b0fd5
vermurachi <vlad.ermurachi@endava.com>
2023-10-02 17:25:41 +0300

    Add Card component, refactor packages

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
-import ExpenseItem from "./components/ExpenseItem";
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2021, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'Pen',
-      amount: 94.12,
-      date: new Date(2021, 7, 14),
-    },
-    {
-      id: 'e3',
-      title: 'New Desk',
-      amount: 98.32,
-      date: new Date(2021, 7, 14),
-    },
-    {
-      id: 'e4',
-      title: 'Notebook',
-      amount: 123.12,
-      date: new Date(2021, 7, 14),
-    }];
-  return (<div>
-    <h2>Let's get started!</h2>
-    <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-    <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-    <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-    <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+import Expenses from "./components/Expenses/Expenses";
 
-  </div>
-  );
+const App = () => {
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2023, 7, 14),
+        },
+        {
+            id: 'e2',
+            title: 'Pen',
+            amount: 94.12,
+            date: new Date(2023, 7, 14),
+        },
+        {
+            id: 'e3',
+            title: 'New Desk',
+            amount: 98.32,
+            date: new Date(2023, 7, 14),
+        },
+        {
+            id: 'e4',
+            title: 'Notebook',
+            amount: 123.12,
+            date: new Date(2023, 7, 14),
+        }];
+    return (<div>
+            <h2>Let's get started!</h2>
+            <Expenses items = {expenses}></Expenses>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,18 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-    return (<div>
-        <div className='expense-item'>{props.date.toISOString}</div>
+import ExpenseDate from "./ExpenseDate";
+
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+    return (
+        <Card className='expense-item'>
+        <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
             <div className='expense-item__price'>{props.amount}</div>
         </div>
-    </div>);
+    </Card>);
 }
 
 export default ExpenseItem;
